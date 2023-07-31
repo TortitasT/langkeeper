b00668869d908f98f3f3e73595a1072033a48f08
TortitasT <victorgf2011@gmail.com>
2023-07-31 22:22:58 +0200

    feat: user create and list

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,74 @@ pub mod schema;
 
 use std::sync::Mutex;
 
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    get, post,
+    web::{self},
+    App, HttpResponse, HttpServer, Responder,
+};
+use diesel::{RunQueryDsl, SqliteConnection};
+use garde::Validate;
+
+use self::models::NewUser;
 
 struct AppState {
-    app_name: String,
-    counter: Mutex<i32>,
+    db: Mutex<SqliteConnection>,
 }
 
-#[get("/")]
-async fn hello(data: web::Data<AppState>) -> impl Responder {
-    HttpResponse::Ok().body(format!(
-        "Hello from {}! Counter: {}",
-        data.app_name,
-        data.counter.lock().unwrap()
-    ))
+#[post("/users")]
+async fn users_new(user: web::Json<NewUser>, app_state: web::Data<AppState>) -> impl Responder {
+    use crate::schema::users;
+
+    let mut conn = app_state.db.lock().unwrap();
+
+    let new_user = models::NewUser {
+        name: user.name.clone(),
+        email: user.email.clone(),
+        password: user.password.clone(),
+    };
+
+    if let Err(error) = new_user.validate(&()) {
+        return HttpResponse::BadRequest().body(error.to_string());
+    }
+
+    let result = diesel::insert_into(users::table)
+        .values(&new_user)
+        .execute(&mut *conn);
+
+    match result {
+        Ok(_) => HttpResponse::Ok().body("User created"),
+        Err(_) => HttpResponse::InternalServerError().body("Something went wrong"),
+    }
+}
+
+#[get("/users")]
+async fn users_index(app_state: web::Data<AppState>) -> impl Responder {
+    use self::schema::users::dsl::users;
+    use diesel::prelude::*;
+
+    let mut conn = app_state.db.lock().unwrap();
+
+    let results = users
+        .limit(5)
+        .load::<models::User>(&mut *conn)
+        .expect("Error loading users");
+
+    HttpResponse::Ok().json(results)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let db = db::establish_connection();
 
-    let app_state = web::Data::new(AppState {
-        app_name: "Actix-web".to_string(),
-        counter: Mutex::new(0),
-    });
+    let app_state = web::Data::new(AppState { db: Mutex::new(db) });
 
-    HttpServer::new(move || App::new().app_data(app_state.clone()).service(hello))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .app_data(app_state.clone())
+            .service(users_new)
+            .service(users_index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,8 @@
 use diesel::prelude::*;
+use garde::{Valid, Validate};
+use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, Selectable)]
+#[derive(Queryable, Selectable, Serialize)]
 #[diesel(table_name = crate::schema::users)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct User {
@@ -10,4 +12,18 @@ pub struct User {
     pub password: String,
     pub created_at: chrono::NaiveDateTime,
     pub updated_at: chrono::NaiveDateTime,
+}
+
+#[derive(Insertable, Deserialize, Serialize, Validate)]
+#[diesel(table_name = crate::schema::users)]
+#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+pub struct NewUser {
+    #[garde(ascii, length(min = 3, max = 20))]
+    pub name: String,
+
+    #[garde(email)]
+    pub email: String,
+
+    #[garde(alphanumeric, length(min = 8))]
+    pub password: String,
 }
