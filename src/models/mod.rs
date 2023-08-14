de88fd3e3cc25171c1d044ebc96c6e5161429258
Victor Garcia <victorgf2011@gmail.com>
2023-08-14 20:30:35 +0200

    feat: languages model, user_language model

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -12,3 +12,27 @@ pub struct User {
     pub created_at: chrono::NaiveDateTime,
     pub updated_at: chrono::NaiveDateTime,
 }
+
+#[derive(Queryable, Selectable, Serialize, Deserialize)]
+#[diesel(table_name = crate::schema::languages)]
+#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+pub struct Language {
+    pub id: i32,
+    pub name: String,
+    pub extension: String,
+    pub created_at: chrono::NaiveDateTime,
+    pub updated_at: chrono::NaiveDateTime,
+}
+
+#[derive(Identifiable, Selectable, Queryable, Associations, Debug)]
+#[diesel(belongs_to(User))]
+#[diesel(belongs_to(Language))]
+#[diesel(table_name = crate::schema::users_languages)]
+#[diesel(primary_key(user_id, language_id))]
+pub struct UserLanguage {
+    pub user_id: i32,
+    pub language_id: i32,
+    pub minutes: i32,
+    pub created_at: chrono::NaiveDateTime,
+    pub updated_at: chrono::NaiveDateTime,
+}
