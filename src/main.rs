26147aa894b9a352dc18fc4671d876ba6a92aa9e
Tortitas <victorgf2011@gmail.com>
2023-08-16 17:00:31 +0200

    feat: port configuration

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ mod jwt;
 #[cfg(test)]
 mod test;
 
+use std::env;
+
 use actix_session::{storage::CookieSessionStore, SessionMiddleware};
 use actix_web::{
     cookie::Key,
@@ -23,7 +25,16 @@ type DbPool = r2d2::Pool<diesel::r2d2::ConnectionManager<SqliteConnection>>;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let address = "0.0.0.0";
-    let port = 8000;
+    let port = match env::args().nth(1) {
+        Some(port) => match port.parse::<u16>() {
+            Ok(port) => port,
+            Err(_) => panic!("Port must be a number"),
+        },
+        None => {
+            println!("No port provided, using default port 8000");
+            8000
+        }
+    };
 
     println!("Starting server...");
     println!("Address: http://{}:{}", address, port);
