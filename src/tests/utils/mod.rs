050e8553d9acc994db240b5c528d26049382f849
Victor Garcia <victorgf2011@gmail.com>
2023-10-08 23:52:03 +0200

    feat: added last monday tests

--- src/tests/utils/mod.rs
+++ src/tests/utils/mod.rs
@@ -3,11 +3,38 @@ use chrono::{DateTime, Local};
 use crate::utils::get_last_monday_date;
 
 #[actix_web::test]
-async fn test_get_last_monday_date() {
-    let mock_date = DateTime::parse_from_rfc3339("2021-01-04T08:00:00+00:00")
+async fn test_get_last_monday_date_on_monday_at_nine() {
+    let mock_date = DateTime::parse_from_rfc3339("2021-01-04T09:00:00+01:00")
         .unwrap()
         .with_timezone(&Local);
     let last_monday = get_last_monday_date(Some(mock_date));
 
     assert_eq!(last_monday, mock_date.naive_local());
+}
+
+#[actix_web::test]
+async fn test_get_last_monday_date_on_monday_at_nine_and_one() {
+    let mock_date = DateTime::parse_from_rfc3339("2021-01-04T09:00:01+01:00")
+        .unwrap()
+        .with_timezone(&Local);
+    let last_monday = get_last_monday_date(Some(mock_date));
+
+    let expected_date = mock_date.date_naive().and_hms_opt(9, 0, 0).unwrap();
+
+    assert_eq!(last_monday, expected_date);
+}
+
+#[actix_web::test]
+async fn test_get_last_monday_date_on_monday_at_eight() {
+    let mock_date = DateTime::parse_from_rfc3339("2021-01-04T08:00:00+01:00")
+        .unwrap()
+        .with_timezone(&Local);
+    let last_monday = get_last_monday_date(Some(mock_date));
+
+    let expected_date = DateTime::parse_from_rfc3339("2020-12-28T09:00:00+01:00")
+        .unwrap()
+        .with_timezone(&Local)
+        .naive_local();
+
+    assert_eq!(last_monday, expected_date);
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, Datelike, Duration, Local, NaiveDateTime};
+use chrono::{DateTime, Datelike, Duration, Local, NaiveDateTime, Timelike};
 
 pub fn get_last_monday_date(override_now: Option<DateTime<Local>>) -> NaiveDateTime {
     let now = match override_now {
@@ -6,7 +6,12 @@ pub fn get_last_monday_date(override_now: Option<DateTime<Local>>) -> NaiveDateT
         None => Local::now(),
     };
 
-    let days_since_monday = now.weekday().num_days_from_monday();
+    let mut days_since_monday = now.weekday().num_days_from_monday();
+
+    if days_since_monday == 0 && now.hour() < 9 {
+        days_since_monday = 7;
+    }
+
     let last_monday = now - Duration::days(days_since_monday.into());
     let last_monday = last_monday.date_naive().and_hms_opt(9, 0, 0);
 
